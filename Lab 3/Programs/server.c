ffc149a4bebefd198cac6732db975d6125c0e9f5
Salman Shah <sbs.191197@gmail.com>
2017-01-19 12:00:25 +0530

    Lab 3 incomplete

---
+++ Lab 3/Programs/server.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fentl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main() {
+	char fname[50], buffer[1025];
+	int req, res,n,file;
+
+	mkfifo("req.fifo",0777);
+	mkfifo("res.fifo",0777);
+
+	printf("Waiting for request...\n");
+
+	req = open("req.fifo", O_RDONLY);
+	res = open("res.fifo", O_WRONLY);
+
+	read(req, fname, sizeof(fname));
+
+	printf("Recieved request for filename%s\n", fname);
+
+	file = open(fname, O_RDONLY);
+
+	if(file < 0)
+		write(res, "File not found\n", 15);
+	else {
+		while((n = read(file, buffer, sizeof(buffer))) > 0) {
+			write(res,buffer, n);
+		}
+	}
+
+	close(req);
+	close(res);
+	unlink("req.fifo");
+	unlink("res.fifo");
+
+	return 0;
+}
